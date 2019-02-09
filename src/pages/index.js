e13917204d41ce02a48d12228c7277700e1f20ee
Tuomas Ukkola <tuomasukk@gmail.com>
2019-02-09 12:55:50 +0200

    fix links layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,13 +15,13 @@ const IndexPage = () => (
       <h1>Tuomas Ukkola</h1>
       <h2>Front End Developer</h2>
       <div className='row'>
-        <div className='col-4 col'>
+        <div className='sm-12 md-4 col'>
           <a href='https://github.com/toumas'>GitHub</a>
         </div>
-        <div className='col-4 col'>
+        <div className='sm-12 md-4 col'>
           <a href='https://www.linkedin.com/in/tuomas-ukkola/'>LinkedIn</a>
         </div>
-        <div className='col-4 col'>
+        <div className='sm-12 md-4 col'>
           <a href='https://codesandbox.io/u/toumas'>CodeSandbox</a>
         </div>
       </div>
